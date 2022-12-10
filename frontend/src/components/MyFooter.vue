8b00688ac2e93d2bc666b385b388662194889f8c
Zbram <75671598+Zbram@users.noreply.github.com>
2022-12-10 14:38:00 +0200

    added footer and header ( blue and green )

--- frontend/src/components/MyFooter.vue
+++ frontend/src/components/MyFooter.vue
@@ -1,16 +1,54 @@
 <template>
-    This is the footer
+    <div class="footer-clean">
+        <footer>
+            <div class="container">
+                <div class="row justify-content-center">
+
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>ADMIN</h3>
+                        <ul>
+                            <li><a><router-link class="rl" to="/admin">ADMIN</router-link></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>TEXTE 2</h3>
+                    </div>
+
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>TEXTE 3</h3>
+                        <ul>
+                            <li><a><router-link to="/about">ABOUT US</router-link></a></li>
+                        </ul>
+                    </div>
+
+                    <div id="bottom-img">
+                        <img src="@/assets/Images/nozama.png" alt="lo" id="img">
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+
 </template>
 
 <script>
 export default {
-    data() {
-        return {
+    name: 'MyFooter',
 
-        }
-    },
     methods: {
+    },
 
-    }
 }
 </script>
+
+<style scoped>
+#img {
+    width: 120px;
+    margin-left: 20px;
+}
+
+footer {
+    background-color : green;
+}
+</style>
--- frontend/src/components/MyHeader.vue
+++ frontend/src/components/MyHeader.vue
@@ -1,30 +1,39 @@
 <template>
-    <div id="logo">
-        <img src="@/assets/Images/nozama.png" alt="lo" id="logo">
-    </div>
-    <header>
-        <nav id="header-nav">
-            <div id="tab-links">
-                <router-link class="rl" to="/">HOME </router-link> |
-                <router-link class="rl" to="/about">ABOUT </router-link> |
-                <router-link class="rl" to="/admin">ADMIN </router-link>
-            </div>
-        </nav>
-    </header>
+    <body>
+        <div id="logo">
+            <img src="@/assets/Images/nozama.png" alt="lo" id="logo">
+        </div>
+        <header>
+            <nav id="header-nav">
+                <div id="tab-links">
+                    <router-link class="rl" to="/">HOME </router-link> |
+                    <router-link class="rl" to="/about">ABOUT </router-link> |
+                    <router-link class="rl" to="/admin">ADMIN </router-link>
+                </div>
+            </nav>
+        </header>
+    </body>
 </template>
 
 <script>
 
 export default {
-  name: 'MyHeader',
+    name: 'MyHeader',
 
-  methods: {
-  },
+    methods: {
+    },
 
 }
 
 </script>
 
 <style scoped>
+#logo {
+    width: 120px;
+    margin-left: 20px;
+}
 
+body {
+    background-color: blue;
+}
 </style>
