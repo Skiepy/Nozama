e28e5433da8958a2e9413d0a66d249ed192069c8
Zbram <75671598+Zbram@users.noreply.github.com>
2022-12-11 16:53:22 +0200

    modif header

--- frontend/src/components/MyHeader.vue
+++ frontend/src/components/MyHeader.vue
@@ -1,18 +1,52 @@
 <template>
-    <body>
-        <div id="logo">
-            <img src="@/assets/Images/nozama.png" alt="lo" id="logo">
-        </div>
-        <header>
-            <nav id="header-nav">
-                <div id="tab-links">
-                    <router-link class="rl" to="/">HOME </router-link> |
-                    <router-link class="rl" to="/about">ABOUT </router-link> |
-                    <router-link class="rl" to="/admin">ADMIN </router-link>
+    <header>
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <div id="headerbar">
+
+            <div class="dropdown"> <!-- 1 -->
+                <button class="bloc-top" @click="scroll">
+                    <span>Products</span>
+                    <img src="@/assets/Images/hamburger.png">
+                </button>
+                <div class="bloc-links">
+                    <ul>
+                        <li>
+                            <a href="#">The Classic</a>
+                            <a href="#"></a>
+                        </li>
+                        <li>
+                            <a href="#">Slippers</a>
+                            <a href="#"></a>
+                        </li>
+                        <li>
+                            <a href="#">Fancy</a>
+                            <a href="#"></a>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </header>
-    </body>
+            </div>
+
+            <div id="logo"> <!-- 2 -->
+                <img src="@/assets/Images/nozama.png" alt="lo" id="logo">
+            </div>
+
+            <div id="searchbar"> <!-- 3 -->
+                <form class="example" action="/action_page.php" style="margin:auto;">
+                    <input type="text" placeholder="Search.." name="search2">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+
+            <div id="cart"> <!-- 4 -->
+
+            </div>
+
+        </div>
+
+
+    </header>
 </template>
 
 <script>
@@ -20,7 +54,29 @@
 export default {
     name: 'MyHeader',
 
+    data() {
+        return {
+
+        };
+    },
+
     methods: {
+        async scroll() {
+            const dropdown = document.querySelector(".dropdown");
+            const btnDrop = document.querySelector(".bloc-top");
+            let toggleIndex = 0;
+
+            btnDrop.addEventListener('click', () => {
+
+                if (toggleIndex == 0) {
+                    dropdown.style.height = `${dropdown.scrollHeight}px`;
+                    toggleIndex++;
+                } else {
+                    dropdown.style.height = `${btnDrop.scrollHeight}px`;
+                    toggleIndex--;
+                }
+            })
+        }
     },
 
 }
@@ -28,12 +84,124 @@ export default {
 </script>
 
 <style scoped>
-#logo {
-    width: 120px;
-    margin-left: 20px;
+/* general header */
+header {
+    z-index: 1;
+    position: sticky;
+    width: 100%;
+    height: 200px;
+    background-color: #131921;
+    overflow: hidden;
+    position: sticky;
+    top: 0;
+    font: 12px Arial;
 }
 
-body {
-    background-color: blue;
+/* the entire header bar (divide nav / logo / search / panier) */
+#headerbar {
+    display: flex;
+    justify-content: space-between;
+    padding-top: 8px;
 }
+
+/* nav */
+
+.dropdown {
+    
+    max-width: 200px;
+    height: 63px;
+    border-radius: 5px;
+    margin-left : 10px;
+    color: #f1f1f1;
+    background: #ffb237;
+    transition: height 0.4s ease-in-out;
+    clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%);
+}
+
+.bloc-top {
+    padding: 20px;
+    display: block;
+    width: 100%;
+    border: none;
+    outline: none;
+    font-size: 20px;
+    display: flex;
+    justify-content: space-between;
+    background: #ffb237;
+    color: #f1f1f1;
+    cursor: pointer;
+    border-radius: 5px;
+}
+.bloc-top img {
+    width: 20px
+}
+.bloc-links {
+    padding: 0 22px;
+    text-align : left;
+}
+.bloc-links li {
+    list-style-type: none;
+    padding: 5px 0;
+}
+.bloc-links li a {
+    text-decoration: none;
+    font-size: 18px;
+    color: #f1f1f1;
+    margin-left: 5px;
+}
+.bloc-links li a:hover {
+    color: rgb(60, 60, 60);
+}
+
+.bloc-links li:last-child {
+    padding-bottom: 25px;
+}
+
+/* logo */
+#logo {
+    width: 120px;
+    margin-left: 10px;
+    margin-top: 2px;
+}
+
+/* search */
+#searchbar * {
+    box-sizing: border-box;
+}
+
+form.example input[type=text] {
+    padding: 10px;
+    padding-left: 20px;
+    font-size: 17px;
+    border: 1px solid grey;
+    float: left;
+    width: 500px;
+    background: #f1f1f1;
+    border-radius: 30px 0 0 30px;
+}
+
+form.example button {
+    float: left;
+    width: 60px;
+    padding: 10px;
+    background: #ffb237;
+    color: white;
+    font-size: 17px;
+    border: 1px solid grey;
+    border-left: none;
+    cursor: pointer;
+    border-radius: 0 30px 30px 0;
+}
+
+form.example button:hover {
+    background: #fc9a00;
+}
+
+form.example::after {
+    content: "";
+    clear: both;
+    display: table;
+}
+
+/* panier */
 </style>
