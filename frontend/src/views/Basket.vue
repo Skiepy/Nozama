af6b3ba75c23c571af64edb23e507830292e5754
Skiepy <72888827+Skiepy@users.noreply.github.com>
2022-12-12 22:35:30 +0100

    Update Basket.vue

--- frontend/src/views/Basket.vue
+++ frontend/src/views/Basket.vue
@@ -1,13 +1,14 @@
 <template>
   <MyHeader></MyHeader>
-  <div>
-    <ul>
-      <li class="col1-item1"> Product: {{ this.productName }}</li>
-  
-      <li class="col2-item1">Description: {{ this.productDescription }}</li>
-  
-      <li class="col3-item1">Price: {{ this.productPrice }} $</li>
-    </ul>
+  <div id="top">
+    <div v-if="(nbProducts == 0)">
+      No products available, we apologize for the inconvenience.
+    </div>
+    <div v-else>
+      <div v-for="product in myProducts">
+        This item {{ product.id_product }} is in the basket.
+      </div>
+    </div>
   </div>
   <MyFooter></MyFooter>
 </template>
@@ -18,30 +19,46 @@ import MyHeader from '@/components/MyHeader.vue';
 import axios from 'axios';
 
 export default {
-    name: "Basket",
-    data() {
-        return {
-            nbProducts: 0,
-            products: []
-        };
+  name: "Basket",
+  data() {
+    return {
+      nbProducts: 0,
+      products: [],
+      myProducts: []
+    };
+  },
+  methods: {
+    async getSessionProducts() {
+      try {
+        const response = await axios.get(`http://localhost:5000/basket`);
+        this.products = response.data;
+        let cpt = 0;
+        for (let index = 0; index < this.products.length; index++) {
+          if (this.products[index].sessionId_basket == window.$cookies.get("sessionId")) {
+            this.myProducts[cpt] = this.products[index];
+            cpt++;
+          }
+        }
+        this.nbProducts = this.myProducts.length;
+      }
+      catch (error) {
+        console.log(error);
+      }
     },
-    methods: {
-        async getAllProducts() {
-            try {
-                const response = await axios.get("http://localhost:5000/products");
-                this.products = response.data;
-                this.nbProducts = this.products.length;
-            }
-            catch (error) {
-                console.log(error);
-            }
-        },
-    },
-    components: { MyHeader, MyFooter }
+  },
+  beforeMount() {
+    // this.getSessionProducts();
+  },
+  components: { MyHeader, MyFooter }
 }
 </script>
 
 <style scoped>
+#top {
+  margin-top: 100px;
+  margin-bottom: 40px;
+}
+
 ul {
   display: grid;
   grid-auto-columns: 1fr;
