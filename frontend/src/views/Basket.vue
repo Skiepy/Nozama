e98d587dac94b04c95105e71688231031ac5c3a1
Zbram <75671598+Zbram@users.noreply.github.com>
2022-12-13 02:18:12 +0200

    Update Basket.vue

--- frontend/src/views/Basket.vue
+++ frontend/src/views/Basket.vue
@@ -1,5 +1,6 @@
 <template>
   <MyHeader></MyHeader>
+
   <div id="top">
     <div v-if="(nbProducts == 0)">
       No products available, we apologize for the inconvenience.
@@ -10,6 +11,51 @@
       </div>
     </div>
   </div>
+
+  <h1> Your Basket</h1>
+  <div id="divtable">
+    <table>
+      <tr>
+        <th>#</th>
+        <th>Product</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Delete</th>
+      </tr>
+
+      <tr>
+        <td>{{ number }}</td>
+        <td>{{ product }}</td>
+        <td>{{ price }}</td>
+        <td><input v-model.number="quantity" type="number" id="quantity" min="1" ></td>
+        <td><button>Delete</button></td>
+      </tr>
+
+      <tr>
+        <td>{{ number }}</td>
+        <td>{{ product }}</td>
+        <td>{{ price }}</td>
+        <td><input v-model.number="quantity" type="number" id="quantity" min="1" ></td>
+        <td><button>Delete</button></td>
+      </tr>
+
+      <tr>
+        <td>{{ number }}</td>
+        <td>{{ product }}</td>
+        <td>{{ price }}</td>
+        <td><input v-model.number="quantity" type="number" id="quantity" min="1" ></td>
+        <td><button>Delete</button></td>
+      </tr>
+
+    </table>
+  </div>
+
+  <div id="buttons">
+    <button id="basket" style="background-color: rgba(255, 0, 0, 0.405)">Clear basket</button>
+    <button id="basket" style="background-color: rgba(34, 255, 0, 0.405)">Purchase items</button>
+  </div>
+
+
   <MyFooter></MyFooter>
 </template>
 
@@ -22,6 +68,11 @@ export default {
   name: "Basket",
   data() {
     return {
+      number: 1,
+      product: "Crocs-example",
+      price: "$10",
+      quantity: 5,
+
       nbProducts: 0,
       products: [],
       myProducts: []
@@ -81,4 +132,68 @@ ul {
 .col3-item1 {
   grid-area: col3-item1;
 }
+
+/* table */
+#divtable {
+  padding-left: 20%;
+  padding-right: 20%;
+  padding-bottom: 40px;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+  box-shadow: 0 2px 15px rgba(64, 64, 64, 0.411);
+  border-radius: 12px 12px 12px 12px;
+  overflow: hidden;
+}
+
+td,th {
+  padding: 15px 15px;
+  text-align: center;
+}
+
+th {
+  background-color: #ffb237;
+  color: #fafafa;
+  font-family: 'Open Sans', Sans-serif;
+  font-weight: 200;
+  text-transform: uppercase;
+}
+
+tr {
+  background-color: #fafafa;
+  font-family: 'Montserrat', sans-serif;
+}
+
+tr:nth-child(even) {
+  background-color: #eeeeee;
+}
+
+td button {
+  color: rgb(0, 170, 255);
+}
+
+button {
+  text-decoration: none;
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+}
+
+#basket{
+  margin : 10px;
+  padding : 10px;
+  border-radius : 5px;
+  box-shadow: 0 0px 5px rgba(64, 64, 64, 0.411); 
+}
+
+#buttons{
+  padding-bottom : 100px;
+}
+
+#quantity{
+  width : 30px;
+}
+
 </style>
